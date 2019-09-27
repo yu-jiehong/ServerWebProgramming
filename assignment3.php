4ed77c57d69a3e2f187857cf5f0e2857d102bd31
Jiehong Yu <13840201+yu-jiehong@users.noreply.github.com>
2019-09-26 22:24:24 -0700

    Update assignment3.php

--- assignment3.php
+++ assignment3.php
@@ -8,17 +8,17 @@ _END;
 
   if($_FILES) {
     $name = $_FILES['filename']['name'];
-		$file_ext = pathinfo($name);
-		if(mime_content_type($name) == 'text/plain' && $file_ext['extension'] == 'txt') //check mime type and extension
-			$ext = 'txt';
-		if($ext) {
-		  move_uploaded_file($_FILES['filename']['tmp_name'], $name);
-		  echo "Uploaded text file '$name'<br>";
-		} else {
-			echo "'$name' is not an accepted file type.<br>";
-			return;
-		}
-		$str = file_get_contents($name);
+    $file_ext = pathinfo($name);
+    if(mime_content_type($name) == 'text/plain' && $file_ext['extension'] == 'txt') //check mime type and extension
+      $ext = 'txt';
+  if($ext) {
+    move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+    echo "Uploaded text file '$name'<br>";
+  } else {
+    echo "'$name' is not an accepted file type.<br>";
+    return;
+  }
+  $str = file_get_contents($name);
     largestProduct($str);
   }
   echo "</body></html>";
@@ -59,15 +59,3 @@ function largestProduct($num) {
     factorial($result[3]) + factorial($result[4]);
   echo "The sum of the factorials are: " . $factsum . "<br>";
 }
-
-function tester_function() {
-  echo "<br>Not sure how to test my code here as I need to start at the top of the file.<br>";
-  echo "I will just list what I tested for.\n";
-  echo "Tested for file extensions and mime types, only txt files are accepted.<br>";
-  echo "Tested for exactly 1000 numbers.<br>";
-  echo "For the provided example, the largest product is 40824.<br>";
-  echo "For the provided example, the sum of the factorials are 40371.<br>";
-  echo "For a file of all zeros, the product was 0 and the factorial sum were 5.<br>";
-}
-
-tester_function();
